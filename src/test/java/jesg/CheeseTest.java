cba2fa7e8ea56d3dac5e598be2ce6820cc37b2bc
jesg <savagejas@gmail.com>
2014-03-31 16:41:56 -0700

    run phantomjs in background

--- src/test/java/jesg/CheeseTest.java
+++ src/test/java/jesg/CheeseTest.java
@@ -1,23 +1,40 @@
 package jesg;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CheeseTest {
-
+    
+    private static PhantomJSDriverService PHANTOMJS_SERVICE;
     private WebDriver driver;
+    
+    @BeforeClass
+    public static void before() throws IOException{
+        PHANTOMJS_SERVICE = PhantomJSDriverService.createDefaultService();
+        PHANTOMJS_SERVICE.start();
+    }
+    
+    @AfterClass
+    public static void cleanUp(){
+        PHANTOMJS_SERVICE.stop();
+    }
 
     @Before
     public void setUp() throws Exception {
-        driver = new PhantomJSDriver();
+        driver = new PhantomJSDriver(PHANTOMJS_SERVICE, DesiredCapabilities.phantomjs());
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
@@ -28,11 +45,19 @@ public class CheeseTest {
 
     @Test
     public void testCheese() {
+        query("cheese");
+    }
+    
+    @Test
+    public void test42() {
+        query("42");
+    }
+    
+    private void query(String query) {
         driver.get("https://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("cheese");
+        driver.findElement(By.name("q")).sendKeys(query);
         driver.findElement(By.name("btnG")).click();
         (new WebDriverWait(driver, 10)).until(ExpectedConditions
-                .titleContains("cheese"));
+                .titleContains(query));
     }
-
 }
